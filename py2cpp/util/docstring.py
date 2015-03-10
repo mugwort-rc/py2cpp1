9ed26a71b5948498c31e94a9a2e7173e5901b3a0
mugwort_rc <mugwort.rc@gmail.com>
2015-03-10 16:53:40 +0900

    Update utils.docstring.detect_type; support the list/dict of ...

--- py2cpp/util/docstring.py
+++ py2cpp/util/docstring.py
@@ -13,13 +13,32 @@ if six.PY3:
 
 from epydoc.markup.restructuredtext import parse_docstring
 
+
+class Type(object):
+    def __init__(self, type, child=None):
+        self.type = type
+        self.child = child
+
+    def __repr__(self):
+        if self.child is not None:
+            return '<{} of {}>'.format(self.type, repr(self.child))
+        return '<{}>'.format(self.type)
+
+    def __eq__(self, rhs):
+        return self.type == rhs.type and self.child == rhs.child
+
+    @staticmethod
+    def create(type):
+        return Type(type)
+
+
 def get_type_hints(docstring):
     errors = []
     parsed = parse_docstring(docstring, errors)
     root,fields = parsed.split_fields()
     result = {
         'params': {},
-        'rtype': 'void',
+        'rtype': Type('void'),
     }
     for field in fields:
         param,type,name = parse_field(field)
@@ -51,5 +70,6 @@ def parse_field(field):
 
 def detect_type(text):
     if text.startswith('list of') or text.startswith('dict of'):
-        raise NotImplemented
-    return text
+        child = detect_type(text[7:].strip())
+        return Type(text[:4], child)
+    return Type.create(text)
--- py2cpp/util/tests/test_docstring.py
+++ py2cpp/util/tests/test_docstring.py
@@ -3,6 +3,7 @@
 import pytest
 
 from .. import docstring
+from ..docstring import Type
 
 DOCSTRINGS = [
     # 0: test_simple_param
@@ -17,6 +18,30 @@ DOCSTRINGS = [
     """
     :rtype: str
     """,
+    # 3: test_list_param
+    """
+    :param list of str x:
+    """,
+    # 4: test_list_type
+    """
+    :type x: list of str
+    """,
+    # 5: test_list_rtype
+    """
+    :rtype: list of str
+    """,
+    # 6: test_dict_param
+    """
+    :param dict of str x:
+    """,
+    # 7: test_dict_type
+    """
+    :type x: dict of str
+    """,
+    # 8: test_dict_rtype
+    """
+    :rtype: dict of str
+    """,
 ]
 
 #===============================================================================
@@ -26,23 +51,73 @@ def test_simple_param():
     result = docstring.get_type_hints(DOCSTRINGS[0])
     assert result == {
         'params': {
-            'x': 'str',
+            'x': Type('str'),
         },
-        'rtype': 'void',
+        'rtype': Type('void'),
     }
 
 def test_simple_type():
     result = docstring.get_type_hints(DOCSTRINGS[1])
     assert result == {
         'params': {
-            'x': 'str',
+            'x': Type('str'),
         },
-        'rtype': 'void',
+        'rtype': Type('void'),
     }
 
 def test_simple_rtype():
     result = docstring.get_type_hints(DOCSTRINGS[2])
     assert result == {
         'params': {},
-        'rtype': 'str',
+        'rtype': Type('str'),
+    }
+
+def test_list_param():
+    result = docstring.get_type_hints(DOCSTRINGS[3])
+    assert result == {
+        'params': {
+            'x': Type('list', Type('str')),
+        },
+        'rtype': Type('void'),
+    }
+
+def test_list_type():
+    result = docstring.get_type_hints(DOCSTRINGS[4])
+    assert result == {
+        'params': {
+            'x': Type('list', Type('str')),
+        },
+        'rtype': Type('void'),
+    }
+
+def test_list_rtype():
+    result = docstring.get_type_hints(DOCSTRINGS[5])
+    assert result == {
+        'params': {},
+        'rtype': Type('list', Type('str')),
+    }
+
+def test_dict_param():
+    result = docstring.get_type_hints(DOCSTRINGS[6])
+    assert result == {
+        'params': {
+            'x': Type('dict', Type('str')),
+        },
+        'rtype': Type('void'),
+    }
+
+def test_dict_type():
+    result = docstring.get_type_hints(DOCSTRINGS[7])
+    assert result == {
+        'params': {
+            'x': Type('dict', Type('str')),
+        },
+        'rtype': Type('void'),
+    }
+
+def test_dict_rtype():
+    result = docstring.get_type_hints(DOCSTRINGS[8])
+    assert result == {
+        'params': {},
+        'rtype': Type('dict', Type('str')),
     }
